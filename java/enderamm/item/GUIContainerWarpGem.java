6b95fd01c9ec5721a4cd39812768d5a7c1a3a1f1
mak326428 <mak326428@gmail.com>
2014-07-16 11:05:37 +0300

    Attempt at making textbox work

--- java/enderamm/item/GUIContainerWarpGem.java
+++ java/enderamm/item/GUIContainerWarpGem.java
@@ -1,10 +1,12 @@
 package enderamm.item;
 
 import cpw.mods.fml.client.FMLClientHandler;
+import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 public class GUIContainerWarpGem extends GuiContainer {
@@ -22,6 +24,58 @@ public class GUIContainerWarpGem extends GuiContainer {
 
     public GUIContainerWarpGem() {
         super(new ContainerWarpGem());
+    }
+
+    @Override
+    public void drawScreen(int par1, int par2, float par3) {
+        super.drawScreen(par1, par2, par3);
+        int xGuiPos = (this.width - this.xSize) / 2; // j
+        int yGuiPos = (this.height - this.ySize) / 2;
+        this.nameTextBox.drawTextBox();
+    }
+
+    @Override
+    public void updateScreen() {
+        this.nameTextBox.updateCursorCounter();
+    }
+
+    @Override
+    protected void keyTyped(char par1, int par2) {
+        if (this.nameTextBox.isFocused()) {
+
+                this.nameTextBox.textboxKeyTyped(par1, par2);
+                if (this.nameTextBox.getText().trim().equals("")) {
+                    this.nameTextBox.setText("");
+                }
+
+
+        }
+        super.keyTyped(par1, par2);
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
+        int xGuiPos = (this.width - this.xSize) / 2; // j
+        int yGuiPos = (this.height - this.ySize) / 2;
+        this.nameTextBox = new GuiTextField(this.fontRendererObj, xGuiPos + 8,
+                yGuiPos + 42, 128, 12);
+        Keyboard.enableRepeatEvents(true);
+        this.nameTextBox.setMaxStringLength(16);
+        this.nameTextBox.setEnableBackgroundDrawing(false);
+        this.nameTextBox.setVisible(true);
+        this.nameTextBox.setFocused(true);
+        this.nameTextBox.setEnableBackgroundDrawing(true);
+        this.nameTextBox.setText("");
+        this.nameTextBox.setCanLoseFocus(false);
+
+    }
+
+    public GuiTextField nameTextBox;
+
+    @Override
+    public void onGuiClosed() {
+        Keyboard.enableRepeatEvents(false);
     }
 
     @Override
