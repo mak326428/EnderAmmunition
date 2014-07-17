3c8b5cddbeaa40a55f9434dac3dca2d6ac3a3078
mak326428 <mak326428@gmail.com>
2014-07-17 12:06:16 +0300

    Recipes update

--- java/enderamm/EAInit.java
+++ java/enderamm/EAInit.java
@@ -1,25 +1,31 @@
 package enderamm;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import enderamm.item.ItemAnnihilationManipulator;
 import enderamm.item.ItemEnderArrow;
 import enderamm.item.ItemEnderArrow.IEnderEffect;
 import enderamm.item.RecipeCombineArrows;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 import java.util.List;
 
 public class EAInit {
     // TODO: uncomment and port when TE is released
     public static void addMatterRecipes() {
-        /*
-        ItemStack matter = new ItemStack(EACommonProxy.itemMaterial.itemID, 1,
+        ItemStack matter = new ItemStack(EACommonProxy.itemMaterial, 1,
 				4);
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Item.diamond,
-				2), "MMM", "M M", "MMM", 'M', matter));
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Item.emerald,
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.diamond,
+                2), "MMM", "M M", "MMM", 'M', matter));
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.emerald,
 				1), "MMM", "MMM", "MMM", 'M', matter));
 		{
 			ItemStack ore = getIfAny("oreCopper", 32);
@@ -46,29 +52,32 @@ public class EAInit {
 						" MM", 'M', matter));
 		}
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
-				Item.glowstone, 28), " MM", "M  ", "   ", 'M', matter));
+                Items.glowstone_dust, 28), " MM", "M  ", "   ", 'M', matter));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
-				Item.enderPearl, 12), " M ", "M  ", "   ", 'M', matter));
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Item.redstone,
+				Items.ender_pearl, 12), " M ", "M  ", "   ", 'M', matter));
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.redstone,
 				56), " M ", "  M", "   ", 'M', matter));
-		GameRegistry
-				.addRecipe(new ShapedOreRecipe(new ItemStack(Block.wood, 12),
-						"   ", " M ", "   ", 'M', matter));
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Block.oreIron,
+        GameRegistry
+                .addRecipe(new ShapedOreRecipe(new ItemStack(Blocks.log, 12),
+                        "   ", " M ", "   ", 'M', matter));
+        GameRegistry
+                .addRecipe(new ShapedOreRecipe(new ItemStack(Items.coal, 56),
+                        "M  ", " M ", "M M", 'M', matter));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Blocks.iron_ore,
 				24), "M M", " M ", "M M", 'M', matter));
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Block.oreGold,
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Blocks.gold_ore,
 				8), " M ", "M M", " M ", 'M', matter));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
-				EACommonProxy.itemMaterial.itemID, 1, 0), "MPM", 'M', matter,
-				'P', new ItemStack(TEItems.itemMaterial, 1, 165)));
+				EACommonProxy.itemMaterial, 1, 0), "MPM", 'M', matter,
+				'P', "dustPyrotheum"));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
-				Block.obsidian, 24, 0), " M ", "M M", "   ", 'M', matter));
+				Blocks.obsidian, 24, 0), " M ", "M M", "   ", 'M', matter));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
-				Block.dragonEgg), "MMM", "MNM", "MMM", 'M', matter, 'N',
-				new ItemStack(Block.beacon)));
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Item.skull, 1,
+				Blocks.dragon_egg), "MMM", "MNM", "MMM", 'M', matter, 'N',
+				new ItemStack(Blocks.beacon)));
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.skull, 1,
 				1), "MMM", "MSM", "MMM", 'M', matter, 'S', new ItemStack(
-				Item.skull, 1, 0)));*/
+				Items.skull, 1, 0)));
     }
 
     public static ItemStack getIfAny(String oreDictName, int stackSize) {
@@ -85,11 +94,11 @@ public class EAInit {
     }
 
     public static void addRecipes() {
-        /*
+
 		// Glass -> Pyrotheum Lens
-		TransposerManager.addFillRecipe(40000, new ItemStack(Block.glass),
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 2),
-				new FluidStack(TEFluids.fluidPyrotheum, 8000), false);
+		//TransposerManager.addFillRecipe(40000, new ItemStack(Blocks.glass),
+		//		new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 2),
+		//		new FluidStack(TEFluids.fluidPyrotheum, 8000), false);
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
 				EACommonProxy.itemMaterial, 1, 1), "NNN", "NNN", "NNN", 'N',
 				new ItemStack(EACommonProxy.itemMaterial, 1, 0)));
@@ -99,33 +108,32 @@ public class EAInit {
 		// HMS
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
 				EACommonProxy.blockHMS, 1, 0), "ENE", "BRB", "EDE", 'E',
-				"ingotEnderium", 'N', new ItemStack(Item.netherStar), 'D',
-				new ItemStack(Block.dragonEgg), 'R', new ItemStack(
-						TEBlocks.blockEnergyCell, 1, 4), 'B', new ItemStack(
-						Block.blockEmerald)));
+				"ingotEnderium", 'N', new ItemStack(Items.nether_star), 'D',
+				new ItemStack(Blocks.dragon_egg), 'R', TEProxy.resonantEnergyCell, 'B', new ItemStack(
+						Blocks.emerald_block)));
 		// Armor Recipes
-		ItemStack energyCapacitor = new ItemStack(TEItems.itemCapacitor, 1, 5);
+		ItemStack energyCapacitor = TEProxy.resonantCapacitor;
 		GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 				EACommonProxy.itemArmorEnderChestplate), "#C#", "###", "###",
-				'#', new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1),
+				'#', new ItemStack(EACommonProxy.itemMaterial, 1, 1),
 				'C', energyCapacitor));
 		GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 				EACommonProxy.itemArmorEnderLeggings), "###", "#C#", "# #",
-				'#', new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1),
+				'#', new ItemStack(EACommonProxy.itemMaterial, 1, 1),
 				'C', energyCapacitor));
 		GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 				EACommonProxy.itemArmorEnderHelmet), "#P#", "#C#", '#',
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1), 'C',
+				new ItemStack(EACommonProxy.itemMaterial, 1, 1), 'C',
 				energyCapacitor, 'P', new ItemStack(
-						EACommonProxy.itemMaterial.itemID, 1, 2)));
+						EACommonProxy.itemMaterial, 1, 2)));
 		GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 				EACommonProxy.itemArmorEnderBoots), "#C#", "# #", '#',
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1), 'C',
+				new ItemStack(EACommonProxy.itemMaterial, 1, 1), 'C',
 				energyCapacitor));
 		GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 				EACommonProxy.itemEnderMagnet), "EPE", "PRP", "EPE", 'E',
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1), 'R',
-				energyCapacitor, 'P', new ItemStack(Item.enderPearl)));
+				new ItemStack(EACommonProxy.itemMaterial, 1, 1), 'R',
+				energyCapacitor, 'P', new ItemStack(Items.ender_pearl)));
 		// GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 		// EACommonProxy.itemMaterial.itemID, 1, 3), " # ", " # ", '#',
 		// new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1)));
@@ -135,36 +143,35 @@ public class EAInit {
 				EACommonProxy.itemWarpGem, 1, OreDictionary.WILDCARD_VALUE)));
 		GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 				EACommonProxy.itemWarpGem), "III", "ERE", "III", 'I',
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1), 'E',
-				new ItemStack(Item.enderPearl), 'R', new ItemStack(
-						TEBlocks.blockEnergyCell, 1, 4)));
+				new ItemStack(EACommonProxy.itemMaterial, 1, 1), 'E',
+				new ItemStack(Items.ender_pearl), 'R', TEProxy.resonantEnergyCell));
 		// Annihilation Manipulator Recipe
 		GameRegistry.addRecipe(new ShapedRFRecipe(ItemAnnihilationManipulator
 				.getAnnihilationManipulator(0), "LJL", "IWI", "IRI", 'L',
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 2), 'W',
+				new ItemStack(EACommonProxy.itemMaterial, 1, 2), 'W',
 				new ItemStack(EACommonProxy.itemWarpGem), 'I', new ItemStack(
-						EACommonProxy.itemMaterial.itemID, 1, 1), 'R',
+						EACommonProxy.itemMaterial, 1, 1), 'R',
 				energyCapacitor, 'J', new ItemStack(
-						EACommonProxy.itemMaterial.itemID, 1, 3)));
+						EACommonProxy.itemMaterial, 1, 3)));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
-				EACommonProxy.itemMaterial.itemID, 1, 3), "ETE", "COC", "ERE",
-				'O', TEBlocks.blockTesseract, 'R', TEItems.itemComponent, 'T',
-				TEItems.itemComponent, 'E', new ItemStack(
-						EACommonProxy.itemMaterial.itemID, 1, 1), 'C',
-				TEItems.itemComponent));
+				EACommonProxy.itemMaterial, 1, 3), "ETE", "COC", "ERE",
+				'O', TEProxy.tesseract, 'R', TEProxy.tesseract, 'T',
+                TEProxy.tesseract, 'E', new ItemStack(
+						EACommonProxy.itemMaterial, 1, 1), 'C',
+                TEProxy.tesseract));
 		GameRegistry.addRecipe(new ShapedRFRecipe(new ItemStack(
 				EACommonProxy.itemHealingGem), "HJR", "EPE", "ECE", 'E',
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 1), 'C',
+				new ItemStack(EACommonProxy.itemMaterial, 1, 1), 'C',
 				energyCapacitor, 'P', new ItemStack(
-						EACommonProxy.itemMaterial.itemID, 1, 2), 'J',
-				new ItemStack(EACommonProxy.itemMaterial.itemID, 1, 3), 'H',
-				new ItemStack(Item.potion.itemID, 1, 8229), 'R', new ItemStack(
-						Item.potion.itemID, 1, 8225)));
+						EACommonProxy.itemMaterial, 1, 2), 'J',
+				new ItemStack(EACommonProxy.itemMaterial, 1, 3), 'H',
+				new ItemStack(Items.potionitem, 1, 8229), 'R', new ItemStack(
+						Items.potionitem, 1, 8225)));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(
 				EACommonProxy.itemEnderBow), "DE ", "DBE", "DE ", 'D',
-				new ItemStack(Block.dragonEgg), 'B',
-				new ItemStack(Item.bow, 1), 'E', new ItemStack(
-						EACommonProxy.itemMaterial.itemID, 1, 1)));*/
+				new ItemStack(Blocks.dragon_egg), 'B',
+				new ItemStack(Items.bow, 1), 'E', new ItemStack(
+						EACommonProxy.itemMaterial, 1, 1)));
         EnderEffects.init();
         for (IEnderEffect eff : ItemEnderArrow.registeredEffects) {
             ItemStack target = new ItemStack(EACommonProxy.itemEnderArrow);
--- java/enderamm/TEProxy.java
+++ java/enderamm/TEProxy.java
@@ -1,7 +1,9 @@
 package enderamm;
 
 import cofh.api.energy.IEnergyContainerItem;
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -9,11 +11,17 @@ import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 
 public class TEProxy {
+    /**
+     * TODO: these are just placeholders for now. CHANGE WHEN TE GETS RELEASED YA!
+     */
     public static CreativeTabs tabTETools = CreativeTabs.tabTools;
     public static CreativeTabs tabTEBlocks = CreativeTabs.tabBlock;
     public static Fluid fluidRedstone = FluidRegistry.WATER;
     public static Fluid fluidGlowstone = FluidRegistry.LAVA;
     public static Fluid fluidEnder = FluidRegistry.WATER;
+    public static ItemStack resonantEnergyCell = new ItemStack(Blocks.diamond_block);
+    public static ItemStack tesseract = new ItemStack(Blocks.gold_block);
+    public static ItemStack resonantCapacitor = new ItemStack(Items.ender_pearl);
 
     public static final String NBT_ENERGY_TAG = "Energy";
 
