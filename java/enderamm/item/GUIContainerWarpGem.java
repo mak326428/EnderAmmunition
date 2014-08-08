f36c4eba01e4c33104fa686767b098602d321eb0
mak326428 <mak326428@gmail.com>
2014-08-08 10:40:43 +0300

    This should work better

--- java/enderamm/item/GUIContainerWarpGem.java
+++ java/enderamm/item/GUIContainerWarpGem.java
@@ -111,23 +111,24 @@ public class GUIContainerWarpGem extends GuiContainer {
         if (curSel != null) {
             xC = 10;
             yC = 60;
-            int index = ItemWarpGem.getAllPoints(getStack()).indexOf(curSel);
+            int index = ItemWarpGem.getAllPoints(getStack()).indexOf(curSel) + 1;
             yC += index * fontRendererObj.FONT_HEIGHT;
             drawTexturedModalRect(xStartGUI + xC, yStartGUI + yC, 0, 191, 128, fontRendererObj.FONT_HEIGHT);
         }
-        {
-            xC = 10; yC = 60;
-            List<ItemWarpGem.WarpPoint> list = ItemWarpGem.getAllPoints(getStack());
 
-            Rectangle area = new Rectangle(10, 60, 130, 100);
-            if (area.contains(mp)) {
-                int yReq = mp.y - yC;
-                int itemID = yReq / fontRendererObj.FONT_HEIGHT;
-                if (itemID < list.size()) {
-                    drawTexturedModalRect(xStartGUI + xC, yStartGUI + itemID * fontRendererObj.FONT_HEIGHT, 0, 181, 128, fontRendererObj.FONT_HEIGHT);
-                }
+        xC = 10;
+        yC = 60;
+        List<ItemWarpGem.WarpPoint> list = ItemWarpGem.getAllPoints(getStack());
+
+        Rectangle area = new Rectangle(10, 60, 130, 100);
+        if (area.contains(mp)) {
+            int yReq = mp.y - yC;
+            int itemID = yReq / fontRendererObj.FONT_HEIGHT;
+            if (itemID < list.size()) {
+                drawTexturedModalRect(xStartGUI + xC, yStartGUI + yC + itemID * fontRendererObj.FONT_HEIGHT, 0, 181, 128, fontRendererObj.FONT_HEIGHT);
             }
         }
+
         if (addButtonEnabled) {
             drawTexturedModalRect(xStartGUI + (int) addButtonRectangle.getX(), yStartGUI + (int) addButtonRectangle.getY(), 208, 128, addButtonRectangle.width, addButtonRectangle.height);
             if (addButtonRectangle.contains(mp))
@@ -143,8 +144,9 @@ public class GUIContainerWarpGem extends GuiContainer {
             if (teleportButtonRectangle.contains(mp))
                 drawTexturedModalRect(xStartGUI + (int) teleportButtonRectangle.getX(), yStartGUI + (int) teleportButtonRectangle.getY(), 208, 212, teleportButtonRectangle.width, teleportButtonRectangle.height);
         }
-        List<ItemWarpGem.WarpPoint> list = ItemWarpGem.getAllPoints(getStack());
-        xC = 10; yC = 60;
+        //List<ItemWarpGem.WarpPoint> list = ItemWarpGem.getAllPoints(getStack());
+        xC = 10;
+        yC = 60;
         for (ItemWarpGem.WarpPoint wp : list) {
             fontRendererObj.drawString(wp.name, xStartGUI + xC, yStartGUI + yC, 0xFFFFFF);
             yC += fontRendererObj.FONT_HEIGHT;
